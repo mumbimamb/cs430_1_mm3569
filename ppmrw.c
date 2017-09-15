8059e20048534cc12333363ab39334622cd16bfb
Mumbi Macheho-Mbuthia <mumbimamb@gmail.com>
2017-09-14 18:43:44 -0700

    able to read header and partially implemented reading of p6 format in readInputFile method

--- ppmrw.c
+++ ppmrw.c
@@ -1,6 +1,10 @@
-// libraries to include for program
+// Libraries to Include
 #include "stdio.h"
 #include "stdlib.h"
+#include "ctype.h" // contains 'isspace method'
+
+// Function Declarations
+void readInputFile(int ppm_format, char* input_file_name);
 
 // the main function of the program which runs when the program is called
 int main (int argc, char* argv[]) {
@@ -38,7 +42,7 @@ int main (int argc, char* argv[]) {
   int error_int = 0; // specifices if an error occured while running read and wrtie functions; if no errors it should be equal to 0
   char* error_message = "Blank"; // specifies an erorr message if an error reached during read and write functions
   // Variables for read and write functions
-  int ppm_format = atoi(argv[1]);
+  int ppm_format = atoi(argv[1]); // the ppm format given in the arguments
 
   //
   /* End of Necessary Variable Declartions */
@@ -48,10 +52,13 @@ int main (int argc, char* argv[]) {
   /* Start of call to functions to read and write data */
   //
   // Call to method to read in data from input file
-  printf("\nReading from the input file specified in the arguments.\n");
+  printf("\nReading from the input file, %s, specified in the arguments.\n", argv[2]);
+
+  readInputFile(ppm_format, argv[2]);
+  // Call to appropriate converter
 
   // Call to method to write data into output file
-  printf("\nNow writing the data to the specified output file.\n");
+  printf("\nNow writing the data to the specified output file, %s.\n", argv[3]);
   //
   /* End of functions to read and write data */
   //
@@ -61,29 +68,120 @@ int main (int argc, char* argv[]) {
 }
 
 /*
-The file handling occurs in the function
+Reads the file given in the arguments as the input.
+All file handling occurs in the function in order to keep everything cohesive.
 */
 void readInputFile(int ppm_format, char* input_file_name){
+  FILE* input_file = fopen(input_file_name, "r");
 
+  // get length of input file
+  fseek(input_file, 0, SEEK_END);
+  long file_length = ftell(input_file);
+  char buffer[file_length];
+
+  ///
+  // Reading header information
+  ///
+
+  // find the magic number first, should be the first thing in file
+  int magic_num;
+  fseek(input_file, 0, SEEK_SET); // set pointer to beginning of file
+  char f_result[100];
+  fscanf(input_file, "%s", f_result);
+  magic_num = f_result[1];
+
+  // find the width, height, and max color value, in the order stated
+  int input_width = -1;
+  int input_height = -1;
+  int input_max_color = -1;
+  char pending_input[15]; // where info sits while sifting through header, character by character
+  pending_input[0] = 'N'; // sign that no input is being gathered at the moment
+  int pending_index = 0;
+
+  int loop_var = 80;
+  while (loop_var >= 0) {
+    f_result[0] = fgetc(input_file); // grabs the first
+
+    if (f_result[0] == '#') {
+      fgets(f_result, 100, input_file);
+    }
+    else if (isspace(f_result[0])) {
+      if (pending_input[0] != 'N') {
+        if (input_width == -1) {
+          input_width = atoi(pending_input);
+        }
+        else if (input_height == -1) {
+          input_height = atoi(pending_input);
+        }
+        else if (input_max_color == -1) {
+          input_max_color = atoi(pending_input);
+          break; // break out of loop
+        }
+        // reset pending input array
+        for (int i = 0; i < pending_index; i = i + 1) {
+          pending_input[i] = ' ';
+        }
+        pending_index = 0; // reset value to ready for next input with new index
+        pending_input[0] = 'N'; // reset value to signify that no input being gathered at moment
+      }
+    }
+    else {
+      pending_input[pending_index] = f_result[0];
+      pending_index = pending_index + 1;
+    }
+    loop_var = loop_var - 1;
+  }
+
+  printf("\nSize of: %ld\n", file_length); // works for both files
+
+  ///
+  // Reading data if the magic number is P3
+  ///
+  if (magic_num == '3') {
+    fclose(input_file); // close the input file
+    FILE *p3_input = fopen(input_file_name, "r"); // reopen file
+
+    fclose(p3_input); // close the input file after reading it
+  }
+
+  ///
+  // Reading data if the magic number is P6
+  ///
+  if (magic_num == '6') {
+    fclose(input_file); // close file so you can read it in binary
+
+    FILE* p6_input = fopen(input_file_name, "rb"); // open t
+
+    unsigned char buffer[3];
+
+    fread(buffer, 3, 1, p6_input);
+    printf("\n%d, %d, %d\n", buffer[0], buffer[1], buffer[1]);
+
+    fclose(p6_input);
+  }
+
+  // if magic num not equal to 3 or 6, return null to signify error
 }
 
 /*
-
+Is only called if the data given is in P6 format. If the input file is already in P3 format then it wil be directly written to
+the output file via the 'writeOutputFile' method.
 */
-void convertToP3(int ppm_format){
+void convertToP3(int width, int height, int max_color){
 
 }
 
 /*
+Is only called if the data given is in P3 format. If the input file is already in P6 format then it wil be directly written to
+the output file via the 'writeOutputFile' method.
 */
 void convertToP6(int ppm_format){
 
 }
 
 /*
-This function is used to write the data to the output file.
-Is passed the output file name from the arguments.
-The file hadnling occurs in the function.
+This function is used to write the data to the output file and is passed the output file name from the arguments.
+All file handling occurs in the function in order to keep things cohesive.
 */
 void writeOutputFile(char* output_file_name){
 FILE* output_file = fopen(output_file_name, "w"); // should erase the contents of the output file or make new one if doesn't exist
